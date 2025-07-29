5342a067dad8d76a5d55c49c45918c484b411896
SKS-Student <anishreeks@hotmail.com>
2025-07-29 16:48:07 -0700

    Update Teleop name

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/LobsterCupTeleop/SystemTest/OptimizedTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/LobsterCupTeleop/SystemTest/OptimizedTeleop.java
@@ -21,7 +21,7 @@ import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
 
-@TeleOp(name = "A Wrok", group = "A")
+@TeleOp(name = "Optimized Battery teleop", group = "A")
 public class OptimizedTeleop extends OpMode {
 
     private Follower follower;
