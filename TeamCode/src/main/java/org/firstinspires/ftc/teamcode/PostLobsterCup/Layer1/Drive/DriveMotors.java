c4ad1eeb5a6e38e6a3d5f3a0927abb2237493384
SwiftButShaky <alexmao2019@gmail.com>
2025-08-08 12:03:02 -0700

    chill drive files

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PostLobsterCup/Layer1/Drive/DriveMotors.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PostLobsterCup/Layer1/Drive/DriveMotors.java
@@ -2,15 +2,8 @@ package org.firstinspires.ftc.teamcode.PostLobsterCup.Layer1.Drive;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-
 import org.firstinspires.ftc.robotcore.external.Telemetry;
-
-/**
- * WORK IN PROGRESS :)
- */
 
 public class DriveMotors {
     private DcMotorEx frontRight;
@@ -20,21 +13,76 @@ public class DriveMotors {
 
     private Telemetry telemetry;
 
-    private final int STOP = 0;
-    private final int MOVE_FORWARD = 0;
-    private final int MOVE_BACK = 0;
-
-    public DriveMotors(HardwareMap hardwareMap, Telemetry telemetry, String motorName, String limitSwitchName) {
-        frontRight = hardwareMap.get(DcMotorEx.class, motorName);
-        frontLeft = hardwareMap.get(DcMotorEx.class, motorName);
-        backRight = hardwareMap.get(DcMotorEx.class, motorName);
-        backLeft = hardwareMap.get(DcMotorEx.class, motorName);
+    public DriveMotors(HardwareMap hardwareMap, Telemetry telemetry, String frName, String flName, String brName, String blName) {
+        frontRight = hardwareMap.get(DcMotorEx.class, frName);
+        frontLeft = hardwareMap.get(DcMotorEx.class, flName);
+        backRight = hardwareMap.get(DcMotorEx.class, brName);
+        backLeft = hardwareMap.get(DcMotorEx.class, blName);
 
         this.telemetry = telemetry;
 
+        // Reverse right side so forward is forward
         frontRight.setDirection(DcMotor.Direction.REVERSE);
         backRight.setDirection(DcMotor.Direction.REVERSE);
 
-        
+        setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+
+    /** Sets the same power to all drive motors */
+    public void setAllPower(double power) {
+        frontRight.setPower(power);
+        frontLeft.setPower(power);
+        backRight.setPower(power);
+        backLeft.setPower(power);
+    }
+
+    /** Stops all drive motors */
+    public void stop() {
+        setAllPower(0);
+    }
+
+    /** Mecanum drive control (forward, strafe, turn) */
+    public void mecanumDrive(double forward, double strafe, double turn) {
+        double fl = forward + strafe + turn;
+        double fr = forward - strafe - turn;
+        double bl = forward - strafe + turn;
+        double br = forward + strafe - turn;
+        frontLeft.setPower(fl);
+        frontRight.setPower(fr);
+        backLeft.setPower(bl);
+        backRight.setPower(br);
+    }
+
+    /** Reset all motor encoders */
+    public void resetEncoders() {
+        frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    }
+
+    /** Set all motors to run using encoders */
+    public void runUsingEncoders() {
+        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    /** Set all motors to brake or coast when power is zero */
+    public void setZeroPowerBehavior(DcMotor.ZeroPowerBehavior behavior) {
+        frontRight.setZeroPowerBehavior(behavior);
+        frontLeft.setZeroPowerBehavior(behavior);
+        backRight.setZeroPowerBehavior(behavior);
+        backLeft.setZeroPowerBehavior(behavior);
+    }
+
+    /** Send encoder positions to telemetry */
+    public void reportEncoders() {
+        telemetry.addData("frontRight Encoder", frontRight.getCurrentPosition());
+        telemetry.addData("frontLeft Encoder", frontLeft.getCurrentPosition());
+        telemetry.addData("backRight Encoder", backRight.getCurrentPosition());
+        telemetry.addData("backLeft Encoder", backLeft.getCurrentPosition());
+        telemetry.update();
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PostLobsterCup/Layer2/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PostLobsterCup/Layer2/Drive.java
@@ -1,4 +1,58 @@
 package org.firstinspires.ftc.teamcode.PostLobsterCup.Layer2;
 
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.PostLobsterCup.Layer1.Drive.DriveMotors;
+import org.firstinspires.ftc.teamcode.PostLobsterCup.Layer1.Drive.IMU;
+
 public class Drive {
+
+    private final DriveMotors driveMotors;
+    private final IMU imu;
+
+    public Drive(HardwareMap hardwareMap, Telemetry telemetry) {
+        // Link hardware to DriveMotors
+        driveMotors = new DriveMotors(hardwareMap, telemetry,
+                "frontRight", "frontLeft", "backRight", "backLeft");
+
+        imu = new IMU(); // assuming your IMU class takes these
+    }
+
+    /** Example: drive forward at a given power */
+    public void driveForward(double power) {
+        driveMotors.mecanumDrive(power, 0, 0);
+    }
+
+    /** Example: strafe right at a given power */
+    public void strafeRight(double power) {
+        driveMotors.mecanumDrive(0, power, 0);
+    }
+
+    /** Example: turn at a given power */
+    public void turn(double power) {
+        driveMotors.mecanumDrive(0, 0, power);
+    }
+
+    /** Example: stop */
+    public void stop() {
+        driveMotors.stop();
+    }
+
+    /** Encoder reset utility */
+    public void resetEncoders() {
+        driveMotors.resetEncoders();
+    }
+
+    /** Could be called every loop to update telemetry or heading correction */
+    public void update() {
+        driveMotors.reportEncoders();
+        // could also check IMU heading here if needed
+    }
+
+    /** Placeholder for an autonomous movement */
+    public void driveSomewhere() {
+        // This could be replaced with a sequence of drive commands
+        driveForward(0.5);
+        // add timing or encoder logic here
+    }
 }
