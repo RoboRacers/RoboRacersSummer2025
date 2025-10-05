666806f5609004913498f3cfe6ec3da6c10fbdf8
SKS-Student <anishreeks@hotmail.com>
2025-10-05 13:57:19 -0700

    Edit Config bank later

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/decode/example/configBank.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode.decode.example;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class configBank {
+
+
+    private DcMotorEx frontRight;
+    private DcMotorEx frontLeft;
+    private DcMotorEx backRight;
+    private DcMotorEx backLeft;
+
+    private Telemetry telemetry;
+    public configBank(HardwareMap hardwareMap, Telemetry telemetry){
+        frontRight = hardwareMap.get(DcMotorEx.class, "frName");
+        frontLeft = hardwareMap.get(DcMotorEx.class, "flName");
+        backRight = hardwareMap.get(DcMotorEx.class, "brName");
+        backLeft = hardwareMap.get(DcMotorEx.class, "blName");
+
+        this.telemetry = telemetry;
+
+        // Reverse right side so forward is forward
+        frontRight.setDirection(DcMotor.Direction.REVERSE);
+        backRight.setDirection(DcMotor.Direction.REVERSE);
+    }
+
+
+
+}
